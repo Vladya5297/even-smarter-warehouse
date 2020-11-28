ac1404dfd1a9242737781a8cf1344b75dfa97870
Vladya5297 <123dobro@mail.ru>
2020-11-29 01:30:32 +0300

    feat: table view improved

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Table as AntTable } from 'antd'
 import { Title } from './localComponents/Title/Title'
 import classes from './Table.module.css'
 
 export const Table = ({
   columns = [],
-  dataSource = []
+  dataSource = [],
+  onSelect,
+  selectedRowKeys
 }) => {
+  const onResize = () => {
+    const query = `.${classes['table-wrapper']} .ant-table-content`
+    const elem = document.querySelector(query)
+    elem.style.maxHeight = `${window.innerHeight - 150}px`
+    elem.style.height = 'min-content'
+  }
+  useEffect(() => {
+    window.addEventListener('resize', onResize)
+    onResize()
+    return () => {
+      window.removeEventListener('resize', onResize)
+    }
+  }, [])
   return (
     <div className={classes['table-wrapper']}>
       <AntTable
@@ -17,17 +32,16 @@ export const Table = ({
         title={() => <Title />}
         pagination={{
           position: ['none', 'bottomCenter'],
+          showSizeChanger: true,
           defaultPageSize: 20,
           pageSizeOptions: [20, 50, 100]
         }}
-        scroll={{
-          x: '100vw',
-          y: '75vh'
-        }}
-        rowSelection={{
+        rowSelection={columns.length && {
           type: 'checkbox',
           columnWidth: '40px',
-          fixed: true
+          fixed: true,
+          onSelect: onSelect,
+          selectedRowKeys: selectedRowKeys
         }}
       />
     </div>
